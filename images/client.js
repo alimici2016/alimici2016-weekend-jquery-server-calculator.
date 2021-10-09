2e1f43989ce0a95c469a868036030562d620d9e5
Hamsa Alimci <alimici2016@gmail.com>
2021-10-09 12:50:21 -0500

    Checklist is complete, the interface looks pretty good so far.

---
+++ images/client.js
@@ -0,0 +1,10 @@
+$(ready);
+function ready(){
+    $("#submit").on(`click`, inputVal)
+}
+
+function inputVal (){
+    console.log('inputs are working')
+    $("#num1Input").val()
+    $("#num2Input").val()
+}
