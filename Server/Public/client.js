5ccb0da554cd9826ee84bc565db1925588a5441e
Hamsa Alimci <alimici2016@gmail.com>
2021-10-10 13:15:53 -0500

    Trying some css while I procrastinate dynamically sending the answers to the DOM

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -4,24 +4,24 @@ function ready() {
     $("#submit").on(`click`, sendInput)
     $("#plusBtn").on(`click`, plusBtn)
     $("#minusBtn").on('click', minusBtn)
-    $("#multiplyBtn").on (`click`, multiplyBtn)
+    $("#multiplyBtn").on(`click`, multiplyBtn)
     $("#divisionBtn").on(`click`, divisionBtn)
-    $("#clearBtn").on(`click`, clearInput)
+    // $("#clearBtn").on(`click`, clearInput)
 }
 
 function plusBtn() {
     operator = '+'
 }
-function minusBtn(){
+function minusBtn() {
     operator = '-'
 }
-function multiplyBtn(){
+function multiplyBtn() {
     operator = '*'
 }
-function divisionBtn(){
+function divisionBtn() {
     operator = '/'
 }
-function clearInput (){
+function clearInput() {
     $("#num1Input").val('')
     $("#num2Input").val('')
 }
@@ -35,24 +35,25 @@ function sendInput() {
             operator: operator,
             num2: $("#num2Input").val()
         }
-    }).then(function (response){
+    }).then(function (response) {
         console.log('successfully posted', response)
         //call back the GET associated with this POST
         receivedInput();
         clearInput();
-    }).catch(function() {
+    }).catch(function () {
         alert('post failed', response);
-     })
+    })
 }
 
 function receivedInput() {
     $.ajax({
         method: 'GET',
         url: '/calculation'
-    }).then(function (response){
-        console.log('success', response.total)
-
-        $("#container").html(response.total)
+    }).then(function (response) {
+        console.log('success', response)
+        $("#container").html(response)
+    }).catch(function () {
+        alert('Failed', response)
     })
 }
 
